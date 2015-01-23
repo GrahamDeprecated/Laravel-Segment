481589c337baaf7d6c34d9b9fdd263bded46d470
James Brooks <james@bluebaytravel.co.uk>
2015-01-23 12:21:11 +0000

    Fix formatting, add missing boot/register functions

--- src/SegmentServiceProvider.php
+++ src/SegmentServiceProvider.php
@@ -1,10 +1,28 @@
 <?php
 
-    namespace CachetHQ\Segment;
+namespace CachetHQ\Segment;
 
-    use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-    class SegmentServiceProvider extends ServiceProvider
+class SegmentServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
     {
-
+        //
     }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}

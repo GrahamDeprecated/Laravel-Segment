f0382ba91dfc69c9ea0ae7af1b6ff32298730461
James Brooks <james@bluebaytravel.co.uk>
2015-01-23 16:59:09 +0000

    Remove enabled key for now

--- src/config/config.php
+++ src/config/config.php
@@ -2,11 +2,6 @@
 
 return [
 	/**
-	 * Whether to enable the Segment library or not.
-	 */
-	'enabled'      => false,
-
-	/**
 	 * The key which enables you to write to the Segment.com API.
 	 */
     'write_key'     => '',

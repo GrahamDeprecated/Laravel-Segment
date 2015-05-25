69eddf45217e16f69834b47963a57746fb1ed7b1
Graham Campbell <graham@cachethq.io>
2015-05-25 19:41:01 +0100

    Added missing file headers

--- src/Facades/Segment.php
+++ src/Facades/Segment.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Segment.
+ *
+ * (c) Cachet HQ <support@cachethq.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CachetHQ\Segment\Facades;
 
 use Illuminate\Support\Facades\Facade;
